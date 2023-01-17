1e765d51b2a41d01ac87b59f4c3cb4556d7d1a00
Juliet Colman <juliet@amjula.com>
2023-01-17 20:43:47 +0000

    image_32 class: stores images in full 32-bit RGBA, so will enable blending, filtering, etc.

---
+++ image_32.cpp
@@ -0,0 +1,61 @@
+// "LO RES" Game Prototype
+// (c) Juliet Colman 2017-2022
+
+#include <cassert>
+#include <iostream>
+#include "lodepng.h"
+#include "image_32.h"
+
+bool image_32::load(const std::string& png_file_name)
+{
+  std::vector<unsigned char> data;
+  unsigned int w = 0;
+  unsigned int h = 0;
+  unsigned nError = lodepng::decode(data, w, h, png_file_name);
+  if (nError != 0)
+  {
+    std::cout << "Failed to load " << png_file_name << "\n";
+    return false;
+  }
+
+  assert(data.size() == w * h * 4);
+  m_data = data; 
+
+  m_width = w;
+  m_height = h;
+
+  return true;
+}
+
+bool image_32::is_transparent(int x, int y) const
+{
+  return get_colour(x, y).a < 128; // actually we just need index(x, y) + 3
+}
+
+colour image_32::get_colour(int x, int y) const 
+{
+  int i = index(x, y);
+  return colour(m_data[i], m_data[i + 1], m_data[i + 2], m_data[i + 3]);
+}
+
+void image_32::set_colour(int x, int y, colour c)
+{
+  int i = index(x, y);
+  m_data[i]     = c.r;
+  m_data[i + 1] = c.g;
+  m_data[i + 2] = c.b;
+  m_data[i + 3] = c.a;
+}
+
+void image_32::clear(colour c) 
+{
+  int s = get_width() * get_height() * 4;
+  for (int i = 0; i < s; i += 4)
+  {
+    m_data[i]     = c.r;
+    m_data[i + 1] = c.g;
+    m_data[i + 2] = c.b;
+    m_data[i + 3] = c.a;
+  }
+}
+
---
+++ image_32.h
@@ -0,0 +1,26 @@
+// "LO RES" Game Prototype
+// (c) Juliet Colman 2017-2022
+
+#pragma once
+
+#include "image_n.h"
+
+// * image_32 *
+// Stores image as array of 32-bit RGBA values.
+
+class image_32 : public image_n<4>
+{
+public:
+  // * load *
+  // Load a png image.
+  bool load(const std::string& png_file_name) override;
+
+  bool is_transparent(int x, int y) const override;
+
+  colour get_colour(int x, int y) const override;
+
+  void set_colour(int x, int y, colour ch) override;
+
+  void clear(colour c) override;
+};
+
