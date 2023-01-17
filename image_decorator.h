608059efb9576d4b48bca9760478af2faac02625
Juliet Colman <juliet@amjula.com>
2023-01-17 15:36:27 +0000

    Image decorator base class

---
+++ image_decorator.h
@@ -0,0 +1,36 @@
+// "LO RES" Game Prototype
+// (c) Juliet Colman 2017-2022
+
+#pragma once
+
+#include "image.h"
+
+// * image decorator*
+// Base class for decorators
+
+class image_decorator : public image
+{
+public:
+  image_decorator() = default;
+  image_decorator(p_image child) : m_child(child) {}
+  void set_child(p_image child) { m_child = child; }
+
+  void set_size(int w, int h) override { m_child->set_size(w, h); }
+
+  bool load(const std::string& png_file_name) override { return m_child->load(png_file_name); }
+
+  void set_colour(int x, int y, colour ch) override { m_child->set_colour(x, y, ch); }
+
+  colour get_colour(int x, int y) const override { return m_child->get_colour(x, y); }
+
+  bool is_transparent(int x, int y) const override { return m_child->is_transparent(x, y); }
+
+  void clear(colour c = 0) override { m_child->clear(c); }
+
+  int get_width() const override { return m_child->get_width(); }
+  int get_height() const override { return m_child->get_height(); }
+
+protected:
+  p_image m_child;
+};
+
