518be6bb12de0f041750bc02e93d60d4a88f8af3
Juliet Colman <juliet@amjula.com>
2023-01-18 10:34:51 +0000

    uv type - maybe use this throughout instead of (x, y) params

---
+++ uv.h
@@ -0,0 +1,16 @@
+#pragma once
+
+struct uv
+{
+  uv() = default;
+  uv(int u_, int v_) : u(u_), v(v_) {}
+
+  int u = 0;
+  int v = 0;
+};
+
+inline uv operator+(const uv& uv1, const uv& uv2)
+{
+  return uv(uv1.u + uv2.u, uv1.v + uv2.v);
+}
+
