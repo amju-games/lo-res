e802be9dc6bf35926e5e1b04b6d88b5a893f42cd
Juliet Colman <juliet@amjula.com>
2023-01-30 10:45:02 +0000

    Add an extra ctor

--- image_colour_xform.h
+++ image_colour_xform.h
@@ -11,15 +11,22 @@
 // Transform colour of all texels, adding a colour, and multiplying by 0..1 for each
 //  component.
 // TODO Should we pre- or post-multiply? Should we offer both?
- 
+
 class image_colour_xform : public image_decorator
 {
 public:
   image_colour_xform() = default;
+
   image_colour_xform(p_image child, const colour& add, const alg3::vec4& mult)
   {
     m_child = child;
     m_add = add;
+    m_mult = mult;
+  }
+
+  image_colour_xform(p_image child, const alg3::vec4& mult)
+  {
+    m_child = child;
     m_mult = mult;
   }
 
