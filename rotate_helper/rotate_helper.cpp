f5b296e8035f07227ae028442f91697915a24a12
Juliet Colman <juliet@amjula.com>
2023-02-07 21:41:52 +0000

    Rotate Helper: generates sprite sheets of rotated images with lighting

--- rotate_helper/rotate_helper.cpp
+++ rotate_helper/rotate_helper.cpp
@@ -5,6 +5,7 @@
 #include "image_32.h"
 #include "image_lighting.h"
 #include "image_normal_map.h"
+#include "image_uv_xform.h"
 #include "lodepng.h"
 
 std::shared_ptr<image_32> normal_map_image;
@@ -18,6 +19,7 @@ float deg_to_rad(float r)
 int main(int argc, char** argv)
 {
   std::string normal_map_filename;
+  std::string mult_filename;
   int cells_in_x = 1;
   int cells_in_y = 1;
 
@@ -29,6 +31,11 @@ int main(int argc, char** argv)
     {
       arg++;
       normal_map_filename = argv[arg];
+    }
+    else if (str == "-m")
+    {
+      arg++;
+      mult_filename = argv[arg];
     }
     else if (str == "-cx")
     {
@@ -49,6 +56,16 @@ int main(int argc, char** argv)
     arg++;
   }
 
+  auto mult_image = std::make_shared<image_32>();
+  if (!mult_filename.empty())
+  {
+    if (!mult_image->load(mult_filename))
+    {
+      std::cout << "Failed to load multiplier image \"" << mult_filename << "\"\n";
+      return 1;
+    }
+  }
+
   normal_map_image = std::make_shared<image_32>();
   if (!normal_map_image->load(normal_map_filename))
   {
@@ -57,7 +74,15 @@ int main(int argc, char** argv)
   }
   normal_map = std::make_shared<image_normal_map>(normal_map_image);
 
-  auto lighting = std::make_shared<image_lighting>(normal_map);
+  auto rotating_normal_map = std::make_shared<image_uv_xform>(normal_map);
+
+  auto lighting = std::make_shared<image_lighting>(rotating_normal_map);
+  lighting->set_specular_power(200.f);
+  lighting->set_diffuse_colour(f_colour(.3f, .3f, 1.f).to_colour());
+
+  auto rotating_mult_image = std::make_shared<image_uv_xform>(mult_image);
+
+//  auto composite_image = std::make_shared
 
   int cell_w = normal_map->get_width();
   int cell_h = normal_map->get_height();
@@ -79,12 +104,20 @@ int main(int argc, char** argv)
     {
       int x = j * cell_w;
       int y = i * cell_h;
+    
+      alg3::vec2 centre_of_rot((cell_w - 1) / 2.f, (cell_h - 1) / 2.f);
+      alg3::mat3 m = rotation2D(centre_of_rot, -degs);
+      rotating_mult_image->set_xform(m); 
+      rotating_normal_map->set_xform(m); 
       
-      alg3::vec3 light_dir { cosf(deg_to_rad(degs)), sinf(deg_to_rad(degs)), 1.f };
-      degs += degs_per_frame;
-      lighting->set_light_dir(light_dir);
+      normal_map->set_rotation(alg3::rotation2D(alg3::vec2(0, 0), degs));
 
+      degs += degs_per_frame;
+
+      //blit<overwrite>(rotating_normal_map, out_image, x, y);
       blit<overwrite>(lighting, out_image, x, y);
+
+      blit<overwrite>(rotating_mult_image, out_image, x, y);
     }
   }
 
