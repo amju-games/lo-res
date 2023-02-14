2752eb69a3f9f1720220e8feae22ab53a9dbfcd8
Juliet Colman <juliet@amjula.com>
2023-02-14 12:44:35 +0000

    Split into pages

--- example/main.cpp
+++ example/main.cpp
@@ -32,69 +32,159 @@ const int WINDOW_W = 500;
 const int WINDOW_H = 500;
 
 // Size in virtual pixels of pretent lo-res screen
-const int VIRTUAL_W = 64;
-const int VIRTUAL_H = 64;
+const int VIRTUAL_W = 128;
+const int VIRTUAL_H = 128;
 
 const colour CLEAR_COLOUR = colour(0xff, 0xc0, 0);
 
 using IMAGE_T = image_32;
 
-std::shared_ptr<image_lighting> lighting_example;
-std::shared_ptr<image_sphere_map> sphere_map_example;
-std::shared_ptr<image_normal_map> normal_map;
-p_image generated_normal_map;
-p_image normal_map_img; // loaded from file
-p_image fruit;
-p_image arrow1;
-p_image arrow2;
-std::shared_ptr<image_uv_xform> rotated_arrow;
-p_image blur_dec;
-sprite spr;
+p_image the_screen;
 font my_font;
 
-void draw()
+class page
 {
-  glClear(GL_COLOR_BUFFER_BIT);
+public:
+  page(std::function<void()> draw_func) : m_draw_func(draw_func) {}
+  void draw() { m_draw_func(); }
 
-  static p_image the_screen;
-  if (!the_screen)
+  std::function<void()> m_draw_func;
+};
+
+#define do_once static int once = 1; if (once-- > 0)
+
+int page_num = 0;
+std::vector<page> pages = 
+{
+page([]()
+{
+  static p_image fruit;
+  static p_image arrow1;
+  static p_image arrow2;
+  static std::shared_ptr<image_uv_xform> rotated_arrow;
+  static p_image blur_dec;
+  static sprite spr;
+
+  do_once
   {
-    the_screen = std::make_shared<IMAGE_T>();
-    the_screen->set_size(VIRTUAL_W, VIRTUAL_H);
+    auto blur_filter = std::vector<std::pair<uv, float>>{
+      {{0, 0}, 1},
+      {{1, 0}, 1}, 
+      {{0, 1}, 1}, 
+      {{1, 1}, 1}
+    };
+
+    fruit = std::make_shared<IMAGE_T>();
+    fruit->load("assets/fruit_salad_64.png");
+
+    arrow1 = std::make_shared<IMAGE_T>();
+    arrow1->load("assets/arrow-in-box.png");
+    //  arrow1->load("assets/fruit_salad_32.png");
+
+    // Make a sprite, apply scale then blur
+    const float SCALEX = 2.f;
+    const float SCALEY = 4.f;
+    auto scale_dec = make_scale_xform(SCALEX, SCALEY);
+    scale_dec->set_child(fruit); //arrow1);
+    blur_dec = std::make_shared<image_filter>(scale_dec, blur_filter);
+    spr.set_image(blur_dec);
+    spr.set_num_cells(1, 1);
+    // Rotated image
+    rotated_arrow = std::make_shared<image_uv_xform>(); 
+    rotated_arrow->set_child(fruit);
+    // Make another image, this time blur then scale
+    auto scale_dec_2 = make_scale_xform(SCALEX, SCALEY);
+    p_image blur_dec_2 = std::make_shared<image_filter>(fruit, blur_filter);
+    scale_dec_2->set_child(blur_dec_2);
+    arrow2 = scale_dec_2;
   }
 
-  the_screen->clear(CLEAR_COLOUR); 
-
-  static float angle = 0.f;
-  angle += 1.0f;
-
-/*
   blit<mask_zero_alpha>(arrow2, the_screen, 1, 64);
   my_font.draw<mask_zero_alpha>(the_screen, 1, 130, "BLUR THEN SCALE");
 
+  spr.update(0);
   spr.draw<overwrite>(the_screen, 80, 64);
   my_font.draw<mask_zero_alpha>(the_screen, 80, 130, "SCALE THEN BLUR");
 
 
   alg3::vec2 centre_of_rot((fruit->get_width() - 1) / 2.f, (fruit->get_height() - 1) / 2.f); 
+  static float angle = 0.f;
+  angle += 1.0f;
   alg3::mat3 m = rotation2D(centre_of_rot, angle);
   rotated_arrow->set_xform(m);
 
   blit<additive_blend>(rotated_arrow, the_screen, 1, 150);
   my_font.draw<mask_zero_alpha>(the_screen, 1, 170, "ROTATED");
 
-
   my_font.draw<mask_zero_alpha>(the_screen, 5, 5, "HELLO\n1234567890!@^&*()_+-=<>,.?/\"':;");
-*/
+}),
+
+page([]()
+{
+  static std::shared_ptr<image_sphere_map> sphere_map_example;
+  static std::shared_ptr<image_normal_map> normal_map;
+  static p_image normal_map_img; // loaded from file
+
+  do_once
+  {
+    normal_map_img = std::make_shared<image_32>();
+    normal_map_img->load("assets/sphere_normal_map_32.png");
+    // Decorate image so we can rotate it
+    normal_map = std::make_shared<image_normal_map>(normal_map_img);
+
+    sphere_map_example = std::make_shared<image_sphere_map>();
+    sphere_map_example->add_child(normal_map);
+    p_image fruit_32 = std::make_shared<image_32>();
+    fruit_32->load("assets/fruit_salad_64.png");
+    sphere_map_example->add_child(fruit_32);
+  }
 
   // Rotate normal map
+  static float angle = 0.f;
+  angle += 1.0f;
   normal_map->set_rotation(alg3::rotation2D(alg3::vec2(0, 0), angle));
 
-  blit<mask_zero_alpha>(lighting_example, the_screen, 0, 0);
   blit<mask_zero_alpha>(sphere_map_example, the_screen, 32, 0);
   blit<overwrite>(normal_map_img, the_screen, 0, 32);
-  blit<overwrite>(generated_normal_map, the_screen, 32, 32);
- 
+}),
+
+page([]() 
+{
+  static p_image generated_normal_map;
+  static std::shared_ptr<image_lighting> lighting_example;
+  do_once
+  {
+    generated_normal_map = std::make_shared<image_32>();
+    generated_normal_map->set_size(32, 32);
+    make_sphere_normals(generated_normal_map);
+    p_image mask = std::make_shared<image_32>();
+    mask->set_size(32, 32);
+    fill(mask, solid_colour(colour(0, 0, 0, 0)));
+    draw_ellipse_solid(mask, 16, 16, 14, 12, colour(0xff, 0xff, 0xff, 0xff));
+    // Mask off outside circle
+    auto masker = std::make_shared<image_mask<>>(generated_normal_map, mask);
+    // Destructive, read from and write to generated_normal_map
+    blit<overwrite>(masker, generated_normal_map, 0, 0);
+  
+    lighting_example = std::make_shared<image_lighting>(generated_normal_map);
+    lighting_example->set_light_dir({1.f, 1.f, 2.f}); 
+    lighting_example->set_diffuse_colour({0xff, 0, 0 });
+    lighting_example->set_specular_power(50.f);
+
+  }
+  my_font.draw<mask_zero_alpha>(the_screen, 1, 1, "GENERATED NORMAL MAP AND LIGHTING");
+  blit<overwrite>(generated_normal_map, the_screen, 0, 10);
+  blit<mask_zero_alpha>(lighting_example, the_screen, 32, 10);
+}) 
+
+};
+
+void draw()
+{
+  the_screen->clear(CLEAR_COLOUR); 
+
+  pages[page_num].draw();
+
   //draw_ellipse_solid(the_screen, 30, 30, 30, 20, colour(0xff, 0, 0));
   //draw_line(the_screen, 5, 5, 60, 70, colour(0, 0xff, 0x40));
 
@@ -107,17 +197,14 @@ void draw()
   glutPostRedisplay();
 }
 
-void update()
+void key_down(unsigned char c, int, int)
 {
-  spr.update(0);
-}
-
-// * draw_and_update *
-// Called every frame.
-void draw_and_update()
-{
-  draw();
-  update();
+  page_num++;
+  if (page_num == pages.size())
+  {
+    page_num = 0;
+  }
+  std::cout << "Going to page " << page_num << "\n";
 }
 
 int main(int argc, char** argv)
@@ -126,86 +213,20 @@ int main(int argc, char** argv)
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
   glutInitWindowSize(WINDOW_W, WINDOW_H);
   glutCreateWindow("Hello");
-  glutDisplayFunc(draw_and_update);
+  glutDisplayFunc(draw);
+  glutKeyboardFunc(key_down);
+
+  the_screen = std::make_shared<image_32>();
+  the_screen->set_size(VIRTUAL_W, VIRTUAL_H);
 
   image_8::get_palette().add_colour(colour(0, 0, 0));
   image_8::get_palette().add_colour(CLEAR_COLOUR);
 
-  auto blur_filter = std::vector<std::pair<uv, float>>{
-      {{0, 0}, 1},
-      {{1, 0}, 1}, 
-      {{0, 1}, 1}, 
-      {{1, 1}, 1}
-    };
-
-  fruit = std::make_shared<IMAGE_T>();
-  fruit->load("assets/fruit_salad_64.png");
-
-  arrow1 = std::make_shared<IMAGE_T>();
-  arrow1->load("assets/arrow-in-box.png");
-//  arrow1->load("assets/fruit_salad_32.png");
-
-
-  // Colour xform
-
-
-  // Make a sprite, apply scale then blur
-  const float SCALEX = 2.f;
-  const float SCALEY = 4.f;
-  auto scale_dec = make_scale_xform(SCALEX, SCALEY);
-  scale_dec->set_child(fruit); //arrow1);
-  blur_dec = std::make_shared<image_filter>(scale_dec, blur_filter);
-  spr.set_image(blur_dec);
-  spr.set_num_cells(1, 1);
-
-
-  // Rotated image
-  rotated_arrow = std::make_shared<image_uv_xform>(); 
-  rotated_arrow->set_child(fruit);
-
-
-  // Make another image, this time blur then scale
-  auto scale_dec_2 = make_scale_xform(SCALEX, SCALEY);
-  p_image blur_dec_2 = std::make_shared<image_filter>(fruit, blur_filter);
-  scale_dec_2->set_child(blur_dec_2);
-  arrow2 = scale_dec_2;
-
   // Load a font
-  p_image im2 = std::make_shared<IMAGE_T>();
+  p_image im2 = std::make_shared<image_32>();
   im2->load("assets/font_4x4.png");
   my_font.set_image(im2); //std::make_shared<image_scale>(im2, 2.f));
   my_font.set_num_cells(16, 4);
-
-  // Lighting example
-  normal_map_img = std::make_shared<image_32>();
-  normal_map_img->load("assets/sphere_normal_map_32.png");
-  // Decorate image so we can rotate it
-  normal_map = std::make_shared<image_normal_map>(normal_map_img);
-
-  generated_normal_map = std::make_shared<image_32>();
-  generated_normal_map->set_size(32, 32);
-  make_sphere_normals(generated_normal_map);
-  p_image mask = std::make_shared<image_32>();
-  mask->set_size(32, 32);
-  fill(mask, solid_colour(colour(0, 0, 0, 0)));
-  draw_ellipse_solid(mask, 16, 16, 14, 12, colour(0xff, 0xff, 0xff, 0xff));
-  // Mask off outside circle
-  auto masker = std::make_shared<image_mask<>>(generated_normal_map, mask);
-  // Destructive, read from and write to generated_normal_map
-  blit<overwrite>(masker, generated_normal_map, 0, 0);
- 
-  //lighting_example = std::make_shared<image_lighting>(normal_map_img); 
-  lighting_example = std::make_shared<image_lighting>(generated_normal_map);
-
-  lighting_example->set_light_dir({1.f, 1.f, 2.f}); 
-  lighting_example->set_diffuse_colour({0xff, 0, 0 });
-  lighting_example->set_specular_power(50.f);
-
-  sphere_map_example = std::make_shared<image_sphere_map>();
-  sphere_map_example->add_child(normal_map);
-  p_image fruit_32 = std::make_shared<image_32>();
-  fruit_32->load("assets/fruit_salad_64.png");
-  sphere_map_example->add_child(fruit_32);
 
   glutMainLoop();
 }
