fba3144a3fdf59df24e665918007480ff649a2b0
Juliet Colman <juliet@amjula.com>
2023-02-18 13:44:24 +0000

    Msvc warnings

--- image_scale.cpp
+++ image_scale.cpp
@@ -8,16 +8,16 @@ colour image_scale::get_colour(int u, int v) const
 {
   float su = static_cast<float>(u) / m_scale_x;
   float sv = static_cast<float>(v) / m_scale_y;
-  return m_child->get_colour(su, sv);
+  return m_child->get_colour(static_cast<int>(su + .5f), static_cast<int>(sv + .5f));
 }
 
 int image_scale::get_width() const 
 {
-  return m_scale_x * m_child->get_width();
+  return static_cast<int>(m_scale_x * static_cast<float>(m_child->get_width()));
 }
 
 int image_scale::get_height() const 
 {
-  return m_scale_y * m_child->get_height();
+  return static_cast<int>(m_scale_y * static_cast<float>(m_child->get_height()));
 }
 
