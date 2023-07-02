4b2c306fd2661357ee8b87531f0fef6a0fb20dbf
Juliet Colman <juliet@amjula.com>
2023-07-02 15:21:35 +0100

    Filler improvements

--- filler.h
+++ filler.h
@@ -9,7 +9,15 @@ struct solid_colour
   colour m_colour;
 };
 
-struct noise
+struct noise_colour
+{
+  colour operator()(int x, int y, int w, int h)
+  {
+    return colour(rand() & 0xff, rand() & 0xff, rand() & 0xff, 0xff);
+  }
+};
+
+struct noise_greyscale
 {
   colour operator()(int x, int y, int w, int h)
   {
@@ -19,13 +27,15 @@ struct noise
 };
 
 template<class FILLER>
-void fill(ref_image im, FILLER f)
+void fill(p_image im, FILLER f)
 {
   int w = im->get_width();
   int h = im->get_height();
-  for (int x = 0; x < w; x++)
+
+  // NB we scan horizontally for cache-friendliness!
+  for (int y = 0; y < h; y++)
   {
-    for (int y = 0; y < h; y++)
+    for (int x = 0; x < w; x++)
     {
       im->set_colour(x, y, f(x, y, w, h));
     }
